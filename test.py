83a12063e83fe07b13154b5e5004877fb8d86ed4
Smriti Nayak <smriti9ak989@gmail.com>
2022-06-19 18:02:41 +0530

    Add main file

---
+++ test.py
@@ -0,0 +1,93 @@
+from keras.models import load_model
+import time
+from tensorflow.keras.utils import img_to_array
+from keras.preprocessing import image
+import cv2
+import numpy as np
+import os, random, subprocess
+import vlc
+import sys
+from playsound import playsound
+import serial
+
+face_classifier = cv2.CascadeClassifier(r'F:\SkillLab\Emotion_Detection_CNN-main\haarcascade_frontalface_default.xml')
+classifier = load_model(r'F:\SkillLab\Emotion_Detection_CNN-main\model.h5')
+
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+
+now = time.time()
+future = now+20
+cap = cv2.VideoCapture(0)
+label = ""
+serial_comm = serial.Serial('COM4', 9600)
+serial_comm.timeout = 1
+
+
+while True:
+    _, frame = cap.read()
+    labels = []
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+
+        if np.sum([roi_gray]) != 0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+
+            prediction = classifier.predict(roi)[0]
+            label = emotion_labels[prediction.argmax()]
+            label_position = (x, y)
+            cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            if time.time() > future:
+                cv2.destroyAllWindows()
+                if label == 'Happy':
+                    cap.release()
+                    cv2.destroyAllWindows()
+
+                    randomfile = random.choice(os.listdir("F:/SkillLab/Songs/Happy/"))
+                    print('Lets play a song and continue vibing to your jolly mood' + randomfile)
+                    file = ('F:/SkillLab/Songs/Happy/' + randomfile)
+                    song = vlc.MediaPlayer(file)
+                    song.play()
+                    #time.sleep(100)
+                    serial_comm.write(b"0")
+                    print("sent happy")
+                    time.sleep(0.5)
+                    print(serial_comm.readline().decode('ascii'))
+                    song.stop()
+                    break
+                    sys.exit("Completed Execution")
+
+                elif label == 'Sad':
+                    cap.release()
+                    cv2.destroyAllWindows()
+
+                    randomfile = random.choice(os.listdir("F:/SkillLab/Songs/Sad/"))
+                    print('Lemme help you cheer up!!' + randomfile)
+                    file = ('F:/SkillLab/Songs/Sad/' + randomfile)
+                    song = vlc.MediaPlayer(file)
+                    song.play()
+                    #time.sleep(100)
+                    serial_comm.write(b"1")
+                    print("sent sad")
+                    time.sleep(0.5)
+                    print(serial_comm.readline().decode('ascii'))
+                    sys.exit("Completed Execution")
+                    song.stop()
+
+
+
+        else:
+            cv2.putText(frame, 'No Faces', (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow('Emotion Detector', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+serial_comm.close()
+cap.release()
+cv2.destroyAllWindows()
